f67013b958ea1a1c3700e7fbf7fc487182e39231
Long Nguyen <long.nguyen11288@gmail.com>
2014-11-13 12:51:32 -0500

    Added spaces and org/spaces

--- organizations.go
+++ organizations.go
@@ -2,11 +2,11 @@ package cf
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-// cfAppsResponse describes the Cloud Controller API result for a list of apps
 type OrganizationResponse struct {
 	Count     int                    `json:"total_results"`
 	Pages     int                    `json:"total_pages"`
@@ -24,7 +24,7 @@ type Organization struct {
 }
 
 func (c *Client) ListOrganizations() []Organization {
-	var organizations []Organization
+	var orgs []Organization
 	var orgResp OrganizationResponse
 	r := c.newRequest("GET", "/v2/organizations")
 	resp, err := c.doRequest(r)
@@ -42,7 +42,34 @@ func (c *Client) ListOrganizations() []Organization {
 	}
 	for _, org := range orgResp.Resources {
 		org.Entity.Guid = org.Meta.Guid
-		organizations = append(organizations, org.Entity)
+		orgs = append(orgs, org.Entity)
 	}
-	return organizations
+	return orgs
+}
+
+func (c *Client) OrganizationSpaces(guid string) []Space {
+	var spaces []Space
+	var spaceResp SpaceResponse
+	path := fmt.Sprintf("/v2/organizations/%s/spaces", guid)
+	r := c.newRequest("GET", path)
+	resp, err := c.doRequest(r)
+	if err != nil {
+		log.Printf("Error requesting space %v", err)
+	}
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading space request %v", resBody)
+	}
+
+	err = json.Unmarshal(resBody, &spaceResp)
+	if err != nil {
+		log.Printf("Error space organization %v", err)
+	}
+	for _, space := range spaceResp.Resources {
+		space.Entity.Guid = space.Meta.Guid
+		spaces = append(spaces, space.Entity)
+	}
+
+	return spaces
+
 }
---
+++ spaces.go
@@ -0,0 +1,47 @@
+package cf
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type SpaceResponse struct {
+	Count     int             `json:"total_results"`
+	Pages     int             `json:"total_pages"`
+	Resources []SpaceResource `json:"resources"`
+}
+
+type SpaceResource struct {
+	Meta   Meta  `json:"metadata"`
+	Entity Space `json:"entity"`
+}
+
+type Space struct {
+	Guid string `json:"guid"`
+	Name string `json:"name"`
+}
+
+func (c *Client) ListSpaces() []Space {
+	var spaces []Space
+	var spaceResp SpaceResponse
+	r := c.newRequest("GET", "/v2/Spaces")
+	resp, err := c.doRequest(r)
+	if err != nil {
+		log.Printf("Error requesting Spaces %v", err)
+	}
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading Space request %v", resBody)
+	}
+
+	err = json.Unmarshal(resBody, &spaceResp)
+	if err != nil {
+		log.Printf("Error unmarshalling Space %v", err)
+	}
+	for _, space := range spaceResp.Resources {
+		space.Entity.Guid = space.Meta.Guid
+		spaces = append(spaces, space.Entity)
+	}
+	return spaces
+}
