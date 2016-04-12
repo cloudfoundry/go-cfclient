d41336adb30588d3bdeae7c89581e9eea155b141
Sam Bryant <sam.bryant@fil.com>
2016-04-12 10:29:13 +0100

    amend list spaces to work with multiple pages

--- spaces.go
+++ spaces.go
@@ -9,6 +9,7 @@ import (
 type SpaceResponse struct {
 	Count     int             `json:"total_results"`
 	Pages     int             `json:"total_pages"`
+	NextUrl   string          `json:"next_url"`
 	Resources []SpaceResource `json:"resources"`
 }
 
@@ -48,25 +49,35 @@ func (s *Space) Org() Org {
 
 func (c *Client) ListSpaces() []Space {
 	var spaces []Space
-	var spaceResp SpaceResponse
-	r := c.newRequest("GET", "/v2/spaces")
-	resp, err := c.doRequest(r)
-	if err != nil {
-		log.Printf("Error requesting spaces %v", err)
-	}
-	resBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("Error reading space request %v", resBody)
-	}
 
-	err = json.Unmarshal(resBody, &spaceResp)
-	if err != nil {
-		log.Printf("Error unmarshalling space %v", err)
-	}
-	for _, space := range spaceResp.Resources {
-		space.Entity.Guid = space.Meta.Guid
-		space.Entity.c = c
-		spaces = append(spaces, space.Entity)
+	requestUrl := "/v2/spaces"
+
+	for {
+		var spaceResp SpaceResponse
+		r := c.newRequest("GET", requestUrl)
+		resp, err := c.doRequest(r)
+		if err != nil {
+			log.Printf("Error requesting spaces %v", err)
+		}
+		resBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("Error reading space request %v", resBody)
+		}
+
+		err = json.Unmarshal(resBody, &spaceResp)
+		if err != nil {
+			log.Printf("Error unmarshalling space %v", err)
+		}
+		for _, space := range spaceResp.Resources {
+			space.Entity.Guid = space.Meta.Guid
+			space.Entity.c = c
+			spaces = append(spaces, space.Entity)
+		}
+		requestUrl = spaceResp.NextUrl
+		if requestUrl == "" {
+			break
+		}
+		resp.Body.Close()
 	}
 	return spaces
 }
