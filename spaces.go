4b322322cf90e2d1978c5ea042a7c13b46b07c06
Joshua Carp <jm.carp@gmail.com>
2017-10-06 07:57:43 -0400

    Fix `AllowSSH` type. (#130)
    * Fix `AllowSSH` type.
    
    * Fix `SpaceQuotaDefGuid` type.

--- spaces.go
+++ spaces.go
@@ -19,8 +19,8 @@ type SpaceRequest struct {
 	AuditorGuid        []string `json:"auditor_guids"`
 	DomainGuid         []string `json:"domain_guids"`
 	SecurityGroupGuids []string `json:"security_group_guids"`
-	SpaceQuotaDefGuid  []string `json:"space_quota_definition_guid"`
-	AllowSSH           []string `json:"allow_ssh"`
+	SpaceQuotaDefGuid  string   `json:"space_quota_definition_guid"`
+	AllowSSH           bool     `json:"allow_ssh"`
 }
 
 type SpaceResponse struct {
@@ -44,6 +44,7 @@ type Space struct {
 	OrgURL              string      `json:"organization_url"`
 	OrgData             OrgResource `json:"organization"`
 	QuotaDefinitionGuid string      `json:"space_quota_definition_guid"`
+	AllowSSH            bool        `json:"allow_ssh"`
 	c                   *Client
 }
 
