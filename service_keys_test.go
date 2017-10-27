c204df52ce90b030d478507d7900dc76d84b1312
Ian Delahorne <ian.delahorne@gmail.com>
2017-10-27 10:57:02 -0700

    Add CreateServiceKey (#138)
    * Add CreateServiceKey
    
    * Remove spurious import

--- service_keys_test.go
+++ service_keys_test.go
@@ -106,3 +106,119 @@ func TestGetServiceKeysByGuid(t *testing.T) {
 		So(serviceKeys[1].ServiceInstanceUrl, ShouldEqual, "/v2/service_instances/ecf26687-e176-4784-b181-b3c942fecb62")
 	})
 }
+
+func TestCreateServiceKey(t *testing.T) {
+	Convey("Create a service key succeeds", t, func() {
+		setup(MockRoute{"POST", "/v2/service_keys", postServiceKeysPayload, "", 201, "", nil}, t)
+		defer teardown()
+
+		c := &Config{
+			ApiAddress: server.URL,
+			Token:      "foobar",
+		}
+		client, err := NewClient(c)
+		So(err, ShouldBeNil)
+
+		csr := CreateServiceKeyRequest{
+			Name:                "key1",
+			ServiceInstanceGuid: "ecf26687-e176-4784-b181-b3c942fecb62",
+		}
+
+		key, err := client.CreateServiceKey(csr)
+		So(err, ShouldBeNil)
+
+		So(key.Name, ShouldEqual, "key1")
+		So(key.ServiceInstanceUrl, ShouldEqual, "/v2/service_instances/ecf26687-e176-4784-b181-b3c942fecb62")
+	})
+
+	Convey("Create a service key with parameters succeeds", t, func() {
+		setup(MockRoute{"POST", "/v2/service_keys", postServiceKeysPayload, "", 201, "", nil}, t)
+		defer teardown()
+
+		c := &Config{
+			ApiAddress: server.URL,
+			Token:      "foobar",
+		}
+		client, err := NewClient(c)
+		So(err, ShouldBeNil)
+
+		csr := CreateServiceKeyRequest{
+			Name:                "key1",
+			ServiceInstanceGuid: "ecf26687-e176-4784-b181-b3c942fecb62",
+			Parameters: map[string]interface{}{
+				"read-only":   true,
+				"username":    "user1",
+				"connections": 6,
+			},
+		}
+
+		_, err = client.CreateServiceKey(csr)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("Create a duplicate service key", t, func() {
+		setup(MockRoute{"POST", "/v2/service_keys", postServiceKeysDuplicatePayload, "", 400, "", nil}, t)
+		defer teardown()
+
+		c := &Config{
+			ApiAddress: server.URL,
+			Token:      "foobar",
+		}
+		client, err := NewClient(c)
+		So(err, ShouldBeNil)
+
+		csr := CreateServiceKeyRequest{
+			Name:                "key1",
+			ServiceInstanceGuid: "ecf26687-e176-4784-b181-b3c942fecb62",
+		}
+
+		key, err := client.CreateServiceKey(csr)
+		So(err.Error(), ShouldEqual, "cfclient: error (360001): CF-ServiceKeyNameTaken")
+
+		So(key.Name, ShouldEqual, "")
+	})
+
+	Convey("Gets a bad JSON response", t, func() {
+		setup(MockRoute{"POST", "/v2/service_keys", postServiceKeysBadPayload, "", 201, "", nil}, t)
+		defer teardown()
+
+		c := &Config{
+			ApiAddress: server.URL,
+			Token:      "foobar",
+		}
+		client, err := NewClient(c)
+		So(err, ShouldBeNil)
+
+		csr := CreateServiceKeyRequest{
+			Name:                "key1",
+			ServiceInstanceGuid: "ecf26687-e176-4784-b181-b3c942fecb62",
+		}
+
+		key, err := client.CreateServiceKey(csr)
+		So(err.Error(), ShouldEqual, "unexpected end of JSON input")
+
+		So(key.Name, ShouldEqual, "")
+	})
+
+	Convey("Gets an unexpected HTTP status code", t, func() {
+		setup(MockRoute{"POST", "/v2/service_keys", "", "", 202, "", nil}, t)
+		defer teardown()
+
+		c := &Config{
+			ApiAddress: server.URL,
+			Token:      "foobar",
+		}
+		client, err := NewClient(c)
+		So(err, ShouldBeNil)
+
+		csr := CreateServiceKeyRequest{
+			Name:                "key1",
+			ServiceInstanceGuid: "ecf26687-e176-4784-b181-b3c942fecb62",
+		}
+
+		key, err := client.CreateServiceKey(csr)
+		So(err.Error(), ShouldEqual, "CF API returned with status code 202")
+
+		So(key.Name, ShouldEqual, "")
+	})
+}
