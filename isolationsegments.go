6d109df7dc835dea2d16a5c70d9d7bd3cf78fee2
Caleb Washburn <cwashburn@pivotal.io>
2018-03-26 17:16:59 -0400

    Added isolation segment api calls (#182)
    * Added ability to associate space managers by username and refactored duplicate code
    
    * Added ability to update space configuration
    
    * changed from SpaceRequest to UpdateSpaceRequest as should not be able to change space name or org guid on update
    
    * added ability to list security groups by space
    
    * allow for nil spaceguids when updating a security group
    
    * update to add unbind for running and staging security groups and tests
    
    * Added support for create and update space quota definitions
    
    * Change return type to be pointer for create/update space quotas
    
    * fixing comments
    
    * adding AssignSpaceQuota to allow associating a space quota with a space
    
    * adding ListOrgSpaceQuotas
    
    * Adding ability to assign/remove org billing managers and refactored duplicate code
    
    * adding ability to create/update org quotas
    
    * adding ability to update org
    
    * remove update space request and use same request struct for updating org
    
    * fix update org where org guid needs to be supplied
    
    * change listbillingmanagers to listorgbillingmanagers
    
    * Adding list org private domains
    
    * Adding ability to share/unshare an existing private domain with a different org
    
    * Adding isolation segment guid to space
    
    * Adding isolation segment guid to space request
    
    * Adding ability to query isolation segments by properties
    
    * add methods to client to add/remove org and space isolation segments

--- isolationsegments.go
+++ isolationsegments.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/pkg/errors"
@@ -110,9 +111,9 @@ func (c *Client) GetIsolationSegmentByGUID(guid string) (*IsolationSegment, erro
 	return &IsolationSegment{Name: isr.Name, GUID: isr.GUID, CreatedAt: isr.CreatedAt, UpdatedAt: isr.UpdatedAt, c: c}, nil
 }
 
-func (c *Client) ListIsolationSegments() ([]IsolationSegment, error) {
+func (c *Client) ListIsolationSegmentsByQuery(query url.Values) ([]IsolationSegment, error) {
 	var iss []IsolationSegment
-	requestUrl := "/v3/isolation_segments"
+	requestUrl := "/v3/isolation_segments?" + query.Encode()
 	for {
 		var isr ListIsolationSegmentsResponse
 		r := c.NewRequest("GET", requestUrl)
@@ -149,6 +150,10 @@ func (c *Client) ListIsolationSegments() ([]IsolationSegment, error) {
 	return iss, nil
 }
 
+func (c *Client) ListIsolationSegments() ([]IsolationSegment, error) {
+	return c.ListIsolationSegmentsByQuery(nil)
+}
+
 // TODO listOrgsForIsolationSegments
 // TODO listSpacesForIsolationSegments
 // TODO setDefaultIsolationSegmentForOrg
@@ -166,6 +171,26 @@ func (c *Client) DeleteIsolationSegmentByGUID(guid string) error {
 
 func (i *IsolationSegment) Delete() error {
 	return i.c.DeleteIsolationSegmentByGUID(i.GUID)
+}
+
+func (c *Client) AddIsolationSegmentToOrg(isolationSegmentGUID, orgGUID string) error {
+	isoSegment := IsolationSegment{GUID: isolationSegmentGUID, c: c}
+	return isoSegment.AddOrg(orgGUID)
+}
+
+func (c *Client) RemoveIsolationSegmentFromOrg(isolationSegmentGUID, orgGUID string) error {
+	isoSegment := IsolationSegment{GUID: isolationSegmentGUID, c: c}
+	return isoSegment.RemoveOrg(orgGUID)
+}
+
+func (c *Client) AddIsolationSegmentToSpace(isolationSegmentGUID, spaceGUID string) error {
+	isoSegment := IsolationSegment{GUID: isolationSegmentGUID, c: c}
+	return isoSegment.AddSpace(spaceGUID)
+}
+
+func (c *Client) RemoveIsolationSegmentFromSpace(isolationSegmentGUID, spaceGUID string) error {
+	isoSegment := IsolationSegment{GUID: isolationSegmentGUID, c: c}
+	return isoSegment.RemoveSpace(spaceGUID)
 }
 
 func (i *IsolationSegment) AddOrg(orgGuid string) error {
