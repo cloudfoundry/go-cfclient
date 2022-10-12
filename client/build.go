aa0c1c418bc7a8474e9ed1d9cf5f3894c8233a7d
Shawn Neal <sneal@sneal.net>
2022-10-12 05:44:17 -0700

    Add build listing and filtering

--- client/build.go
+++ client/build.go
@@ -1,64 +1,103 @@
 package client
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-
 	"github.com/cloudfoundry-community/go-cfclient/resource"
 )
 
 type BuildClient commonClient
 
-func (c *BuildClient) Get(buildGUID string) (*resource.Build, error) {
-	resp, err := c.client.DoRequest(c.client.NewRequest("GET", "/v3/builds/"+buildGUID))
-	if err != nil {
-		return nil, fmt.Errorf("error getting build: %w", err)
-	}
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
+const BuildsPath = "/v3/builds"
 
+type BuildListOptions struct {
+	*ListOptions
+
+	States       Filter `filter:"states,omitempty"`
+	AppGUIDs     Filter `filter:"app_guids,omitempty"`
+	PackageGUIDs Filter `filter:"package_guids,omitempty"`
+}
+
+type BuildAppListOptions struct {
+	*ListOptions
+
+	States Filter `filter:"states,omitempty"`
+}
+
+func NewBuildListOptions() *BuildListOptions {
+	return &BuildListOptions{
+		ListOptions: NewListOptions(),
+	}
+}
+
+func NewBuildAppListOptions() *BuildAppListOptions {
+	return &BuildAppListOptions{
+		ListOptions: NewListOptions(),
+	}
+}
+
+func (c *BuildClient) Create(r *resource.BuildCreate) (*resource.Build, error) {
 	var build resource.Build
-	if err := json.NewDecoder(resp.Body).Decode(&build); err != nil {
-		return nil, fmt.Errorf("error reading build JSON: %w", err)
+	err := c.client.post(r.Package.GUID, BuildsPath, r, &build)
+	if err != nil {
+		return nil, err
 	}
-
 	return &build, nil
 }
 
-func (c *BuildClient) Create(packageGUID string, lifecycle *resource.Lifecycle, metadata *resource.Metadata) (*resource.Build, error) {
-	req := c.client.NewRequest("POST", "/v3/builds")
-	params := map[string]interface{}{
-		"package": map[string]interface{}{
-			"guid": packageGUID,
-		},
-	}
-	if lifecycle != nil {
-		params["lifecycle"] = lifecycle
-	}
-	if metadata != nil {
-		params["metadata"] = metadata
-	}
-	req.obj = params
+func (c *BuildClient) Delete(guid string) error {
+	return c.client.delete(joinPath(BuildsPath, guid))
+}
 
-	resp, err := c.client.DoRequest(req)
-	if err != nil {
-		return nil, fmt.Errorf("error while creating v3 build: %w", err)
-	}
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
-
-	if resp.StatusCode != http.StatusCreated {
-		return nil, fmt.Errorf("error creating v3 build, response code: %d", resp.StatusCode)
-	}
-
+func (c *BuildClient) Get(guid string) (*resource.Build, error) {
 	var build resource.Build
-	if err := json.NewDecoder(resp.Body).Decode(&build); err != nil {
-		return nil, fmt.Errorf("error reading build JSON: %w", err)
+	err := c.client.get(joinPath(BuildsPath, guid), &build)
+	if err != nil {
+		return nil, err
 	}
+	return &build, nil
+}
 
+func (c *BuildClient) List(opts *BuildListOptions) ([]*resource.Build, *Pager, error) {
+	var res resource.BuildList
+	err := c.client.get(joinPathAndQS(opts.ToQueryString(opts), BuildsPath), &res)
+	if err != nil {
+		return nil, nil, err
+	}
+	pager := NewPager(res.Pagination)
+	return res.Resources, pager, nil
+}
+
+func (c *BuildClient) ListAll() ([]*resource.Build, error) {
+	opts := NewBuildListOptions()
+	var allBuilds []*resource.Build
+	for {
+		builds, pager, err := c.List(opts)
+		if err != nil {
+			return nil, err
+		}
+		allBuilds = append(allBuilds, builds...)
+		if !pager.HasNextPage() {
+			break
+		}
+		opts.ListOptions = pager.NextPage(opts.ListOptions)
+	}
+	return allBuilds, nil
+}
+
+func (c *BuildClient) ListForApp(appGUID string, opts *BuildAppListOptions) ([]*resource.Build, *Pager, error) {
+	var res resource.BuildList
+	err := c.client.get(joinPathAndQS(opts.ToQueryString(opts), AppsPath, appGUID, "builds"), &res)
+	if err != nil {
+		return nil, nil, err
+	}
+	pager := NewPager(res.Pagination)
+	return res.Resources, pager, nil
+}
+
+func (c *BuildClient) Update(guid string, r *resource.BuildUpdate) (*resource.Build, error) {
+	var build resource.Build
+	err := c.client.patch(joinPath(BuildsPath, guid), r, &build)
+	if err != nil {
+		return nil, err
+	}
 	return &build, nil
 }
--- client/payloads_test.go
+++ client/payloads_test.go
@@ -2365,7 +2365,7 @@ const createAppPayload = `{
   }
 }`
 
-const createBuildPayload = `{
+const buildPayload = `{
   "guid": "585bc3c1-3743-497d-88b0-403ad6b56d16",
   "created_at": "2016-03-28T23:39:34Z",
   "updated_at": "2016-06-08T16:41:26Z",
@@ -2399,6 +2399,120 @@ const createBuildPayload = `{
       "href": "https://api.example.org/v3/apps/7b34f1cf-7e73-428a-bb5a-8a17a8058396"
     }
   }
+}
+`
+
+const buildListPayloadPage1 = `{
+  "pagination": {
+    "total_results": 2,
+    "total_pages": 2,
+    "first": {
+      "href": "https://api.example.org/v3/builds?page=1&per_page=2"
+    },
+    "last": {
+      "href": "https://api.example.org/v3/builds?page=2&per_page=2"
+    },
+    "next": {
+      "href": "https://api.example.org/v3/builds?page=2&per_page=2"
+    },
+    "previous": null
+  },
+  "resources": [
+    {
+      "guid": "585bc3c1-3743-497d-88b0-403ad6b56d16",
+      "created_at": "2016-03-28T23:39:34Z",
+      "updated_at": "2016-06-08T16:41:26Z",
+      "created_by": {
+        "guid": "3cb4e243-bed4-49d5-8739-f8b45abdec1c",
+        "name": "bill",
+        "email": "bill@example.com"
+      },
+      "state": "STAGING",
+      "error": null,
+      "lifecycle": {
+        "type": "buildpack",
+        "data": {
+          "buildpacks": [
+            "ruby_buildpack"
+          ],
+          "stack": "cflinuxfs2"
+        }
+      },
+      "package": {
+        "guid": "8e4da443-f255-499c-8b47-b3729b5b7432"
+      },
+      "droplet": null,
+      "metadata": {
+        "labels": {},
+        "annotations": {}
+      },
+      "links": {
+        "self": {
+          "href": "https://api.example.org/v3/builds/585bc3c1-3743-497d-88b0-403ad6b56d16"
+        },
+        "app": {
+          "href": "https://api.example.org/v3/apps/7b34f1cf-7e73-428a-bb5a-8a17a8058396"
+        }
+      }
+    }
+  ]
+}
+`
+
+const buildListPayloadPage2 = `{
+  "pagination": {
+    "total_results": 2,
+    "total_pages": 2,
+    "first": {
+      "href": "https://api.example.org/v3/builds?page=1&per_page=2"
+    },
+    "last": {
+      "href": "https://api.example.org/v3/builds?page=2&per_page=2"
+    },
+    "previous": {
+      "href": "https://api.example.org/v3/builds?page1&per_page=2"
+    },
+    "next": null
+  },
+  "resources": [
+    {
+      "guid": "787bc3c1-3743-497d-88b0-403ad6b56d23",
+      "created_at": "2016-04-28T23:39:34Z",
+      "updated_at": "2016-04-08T16:41:26Z",
+      "created_by": {
+        "guid": "3cb4e243-bed4-49d5-8739-f8b45abdec1c",
+        "name": "bill",
+        "email": "bill@example.com"
+      },
+      "state": "STAGED",
+      "error": null,
+      "lifecycle": {
+        "type": "buildpack",
+        "data": {
+          "buildpacks": [
+            "ruby_buildpack"
+          ],
+          "stack": "cflinuxfs3"
+        }
+      },
+      "package": {
+        "guid": "994da443-f255-499c-8b47-b3729b5b74ad"
+      },
+      "droplet": null,
+      "metadata": {
+        "labels": {},
+        "annotations": {}
+      },
+      "links": {
+        "self": {
+          "href": "https://api.example.org/v3/builds/787bc3c1-3743-497d-88b0-403ad6b56d23"
+        },
+        "app": {
+          "href": "https://api.example.org/v3/apps/8c34f1cf-7e73-428a-bb5a-8a17a8058396"
+        }
+      }
+    }
+  ]
 }
 `
 
