6ccd3f5b6f7e70731de448031d43a29df5f06049
Shawn Neal <sneal@sneal.net>
2022-10-26 17:54:07 -0700

    Add fully implemented domain resource

--- client/domains.go
+++ client/domains.go
@@ -1,51 +1,122 @@
 package client
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-
 	"github.com/cloudfoundry-community/go-cfclient/resource"
 )
 
 type DomainClient commonClient
 
-func (c *DomainClient) ListByQuery(query url.Values) ([]resource.Domain, error) {
-	var domains []resource.Domain
-	requestURL := "/v3/domains"
-	if e := query.Encode(); len(e) > 0 {
-		requestURL += "?" + e
+// DomainListOptions list filters
+type DomainListOptions struct {
+	*ListOptions
+
+	GUIDs             Filter `filter:"guids,omitempty"`
+	Names             Filter `filter:"names,omitempty"`
+	OrganizationGUIDs Filter `filter:"organization_guids,omitempty"`
+}
+
+// NewDomainListOptions creates new options to pass to list
+func NewDomainListOptions() *DomainListOptions {
+	return &DomainListOptions{
+		ListOptions: NewListOptions(),
 	}
+}
 
+// Create a new domain
+func (c *DomainClient) Create(r *resource.DomainCreate) (*resource.Domain, error) {
+	var d resource.Domain
+	err := c.client.post(r.Name, "/v3/domains", r, &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+// Delete the specified app
+func (c *DomainClient) Delete(guid string) error {
+	return c.client.delete(path("/v3/domains/%s", guid))
+}
+
+// Get the specified domain
+func (c *DomainClient) Get(guid string) (*resource.Domain, error) {
+	var d resource.Domain
+	err := c.client.get(path("/v3/domains/%s", guid), &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+// List all Domains the user has access to in paged results
+func (c *DomainClient) List(opts *DomainListOptions) ([]*resource.Domain, *Pager, error) {
+	var res resource.DomainList
+	err := c.client.get(path("/v3/domains?%s", opts.ToQueryString(opts)), &res)
+	if err != nil {
+		return nil, nil, err
+	}
+	pager := NewPager(res.Pagination)
+	return res.Resources, pager, nil
+}
+
+// ListAll retrieves all Domains the user has access to
+func (c *DomainClient) ListAll() ([]*resource.Domain, error) {
+	opts := NewDomainListOptions()
+	var allDomains []*resource.Domain
 	for {
-		resp, err := c.client.DoRequest(c.client.NewRequest("GET", requestURL))
+		Domains, pager, err := c.List(opts)
 		if err != nil {
-			return nil, fmt.Errorf("error getting domains: %w", err)
+			return nil, err
 		}
-		defer func(b io.ReadCloser) {
-			_ = b.Close()
-		}(resp.Body)
-
-		if resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("error listing v3 app domains, response code: %d", resp.StatusCode)
-		}
-
-		var data resource.ListDomainsResponse
-		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-			return nil, fmt.Errorf("error parsing JSON from list v3 app domains: %w", err)
-		}
-
-		domains = append(domains, data.Resources...)
-		requestURL = data.Pagination.Next.Href
-		if requestURL == "" || query.Get("page") != "" {
+		allDomains = append(allDomains, Domains...)
+		if !pager.HasNextPage() {
 			break
 		}
-		requestURL, err = extractPathFromURL(requestURL)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing the next page request url for v3 domains: %w", err)
-		}
+		opts.ListOptions = pager.NextPage(opts.ListOptions)
 	}
-	return domains, nil
+	return allDomains, nil
+}
+
+// ListForOrg retrieves all domains for the specified org that the user has access to in paged results
+func (c *DomainClient) ListForOrg(orgGUID string, opts *DomainListOptions) ([]*resource.Domain, *Pager, error) {
+	var res resource.DomainList
+	err := c.client.get(path("/v3/organizations/%s/domains?%s", orgGUID, opts.ToQueryString(opts)), &res)
+	if err != nil {
+		return nil, nil, err
+	}
+	pager := NewPager(res.Pagination)
+	return res.Resources, pager, nil
+}
+
+// Share an organization-scoped domain to the organization specified by the org guid
+// This will allow the organization to use the organization-scoped domain
+func (c *DomainClient) Share(domainGUID, orgGUID string) (*resource.ToManyRelationships, error) {
+	r := resource.NewDomainShare(orgGUID)
+	return c.ShareMany(domainGUID, r)
+}
+
+// ShareMany shares an organization-scoped domain to other organizations specified by a list of organization guids
+// This will allow any of the other organizations to use the organization-scoped domain.
+func (c *DomainClient) ShareMany(guid string, r *resource.ToManyRelationships) (*resource.ToManyRelationships, error) {
+	var d resource.ToManyRelationships
+	err := c.client.post(guid, path("/v3/domains/%s/relationships/shared_organizations", guid), r, &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+// Share an organization-scoped domain to other organizations specified by a list of organization guids
+// This will allow any of the other organizations to use the organization-scoped domain.
+func (c *DomainClient) Unshare(domainGUID, orgGUID string) error {
+	return c.client.delete(path("/v3/domains/%s/relationships/shared_organizations/%s", domainGUID, orgGUID))
+}
+
+// Update the specified attributes of the app
+func (c *DomainClient) Update(guid string, r *resource.DomainUpdate) (*resource.Domain, error) {
+	var d resource.Domain
+	err := c.client.patch(path("/v3/domains/%s", guid), r, &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
 }
