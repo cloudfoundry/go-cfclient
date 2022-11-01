2e3d2497da039fa579c65bca4d7af62684dc5288
Shawn Neal <sneal@sneal.net>
2022-11-01 15:21:08 -0700

    Remove v2 error types

--- client/error.go
+++ client/error.go
@@ -4,43 +4,29 @@ package client
 
 import (
 	"fmt"
+	"strings"
 )
 
+type CloudFoundryErrors struct {
+	Errors []CloudFoundryError `json:"errors"`
+}
+
+func (e CloudFoundryErrors) Error() string {
+	var sb strings.Builder
+	for _, err := range e.Errors {
+		sb.WriteString(err.Error())
+	}
+	return sb.String()
+}
+
 type CloudFoundryError struct {
-	Code        int    `json:"code"`
-	ErrorCode   string `json:"error_code"`
-	Description string `json:"description"`
-}
-
-type CloudFoundryErrorsV3 struct {
-	Errors []CloudFoundryErrorV3 `json:"errors"`
-}
-
-type CloudFoundryErrorV3 struct {
 	Code   int    `json:"code"`
 	Title  string `json:"title"`
 	Detail string `json:"detail"`
 }
 
-// CF APIs v3 can return multiple errors, we take the first one and convert it into a V2 model
-func NewCloudFoundryErrorFromV3Errors(cfErrorsV3 CloudFoundryErrorsV3) CloudFoundryError {
-	if len(cfErrorsV3.Errors) == 0 {
-		return CloudFoundryError{
-			0,
-			"GO-Client-No-Errors",
-			"No Errors in response from V3",
-		}
-	}
-
-	return CloudFoundryError{
-		cfErrorsV3.Errors[0].Code,
-		cfErrorsV3.Errors[0].Title,
-		cfErrorsV3.Errors[0].Detail,
-	}
-}
-
-func (cfErr CloudFoundryError) Error() string {
-	return fmt.Sprintf("cfclient error (%s|%d): %s", cfErr.ErrorCode, cfErr.Code, cfErr.Description)
+func (e CloudFoundryError) Error() string {
+	return fmt.Sprintf("cfclient error (%s|%d): %s", e.Title, e.Code, e.Detail)
 }
 
 type CloudFoundryHTTPError struct {
