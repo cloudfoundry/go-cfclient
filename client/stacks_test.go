9c0486802b6765f43fbed028470673c3e473d84b
Shawn Neal <sneal@sneal.net>
2022-11-01 13:59:08 -0700

    Add updated stacks client and tests

--- client/stacks_test.go
+++ client/stacks_test.go
@@ -1,32 +1,120 @@
 package client
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/cloudfoundry-community/go-cfclient/resource"
+	"github.com/cloudfoundry-community/go-cfclient/test"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"testing"
 )
 
-func TestListStacksByQuery(t *testing.T) {
-	mocks := []MockRoute{
-		{"GET", "/v3/stacks", []string{listStacksPayload}, "", http.StatusOK, "", ""},
+func TestStacks(t *testing.T) {
+	g := test.NewObjectJSONGenerator(1)
+	stack := g.Stack()
+	stack2 := g.Stack()
+
+	tests := []RouteTest{
+		{
+			Description: "Create stack",
+			Route: MockRoute{
+				Method:   "POST",
+				Endpoint: "/v3/stacks",
+				Output:   []string{stack},
+				Status:   http.StatusCreated,
+				PostForm: `{ "name": "my-stack", "description": "Here is my stack!" }`,
+			},
+			Expected: stack,
+			Action: func(c *Client, t *testing.T) (any, error) {
+				stackDescription := "Here is my stack!"
+				r := &resource.StackCreate{
+					Name:        "my-stack",
+					Description: &stackDescription,
+				}
+				return c.Stacks.Create(r)
+			},
+		},
+		{
+			Description: "Delete stack",
+			Route: MockRoute{
+				Method:   "DELETE",
+				Endpoint: "/v3/stacks/88db2b75-671f-4e4b-a19a-7db992366595",
+				Status:   http.StatusAccepted,
+			},
+			Action: func(c *Client, t *testing.T) (any, error) {
+				return nil, c.Stacks.Delete("88db2b75-671f-4e4b-a19a-7db992366595")
+			},
+		},
+		{
+			Description: "Get stack",
+			Route: MockRoute{
+				Method:   "GET",
+				Endpoint: "/v3/stacks/88db2b75-671f-4e4b-a19a-7db992366595",
+				Output:   []string{stack},
+				Status:   http.StatusOK},
+			Expected: stack,
+			Action: func(c *Client, t *testing.T) (any, error) {
+				return c.Stacks.Get("88db2b75-671f-4e4b-a19a-7db992366595")
+			},
+		},
+		{
+			Description: "List all stacks",
+			Route: MockRoute{
+				Method:   "GET",
+				Endpoint: "/v3/stacks",
+				Output:   g.Paged([]string{stack}, []string{stack2}),
+				Status:   http.StatusOK},
+			Expected: g.Array(stack, stack2),
+			Action: func(c *Client, t *testing.T) (any, error) {
+				return c.Stacks.ListAll(nil)
+			},
+		},
+		{
+			Description: "Update stack",
+			Route: MockRoute{
+				Method:   "PATCH",
+				Endpoint: "/v3/stacks/88db2b75-671f-4e4b-a19a-7db992366595",
+				Output:   []string{stack},
+				Status:   http.StatusOK,
+				PostForm: `{ "metadata": { "labels": { "key": "value" }, "annotations": {"note": "detailed information"}}}`,
+			},
+			Expected: stack,
+			Action: func(c *Client, t *testing.T) (any, error) {
+				r := &resource.StackUpdate{
+					Metadata: &resource.Metadata{
+						Labels: map[string]string{
+							"key": "value",
+						},
+						Annotations: map[string]string{
+							"note": "detailed information",
+						},
+					},
+				}
+				return c.Stacks.Update("88db2b75-671f-4e4b-a19a-7db992366595", r)
+			},
+		},
 	}
-	setupMultiple(mocks, t)
-	defer teardown()
+	for _, tt := range tests {
+		func() {
+			setup(tt.Route, t)
+			defer teardown()
+			details := fmt.Sprintf("%s %s", tt.Route.Method, tt.Route.Endpoint)
+			if tt.Description != "" {
+				details = tt.Description + ": " + details
+			}
 
-	c, _ := NewTokenConfig(server.URL, "foobar")
-	client, err := New(c)
-	require.NoError(t, err)
+			c, _ := NewTokenConfig(server.URL, "foobar")
+			cl, err := New(c)
+			require.NoError(t, err, details)
 
-	stacks, err := client.Stacks.ListByQuery(nil)
-	require.NoError(t, err)
-	require.Len(t, stacks, 2)
-
-	require.Equal(t, "my-stack-1", stacks[0].Name)
-	require.Equal(t, "This is my first stack!", stacks[0].Description)
-	require.Equal(t, "guid-1", stacks[0].GUID)
-	require.Equal(t, "https://api.example.org/v3/stacks/guid-1", stacks[0].Links["self"].Href)
-	require.Equal(t, "my-stack-2", stacks[1].Name)
-	require.Equal(t, "This is my second stack!", stacks[1].Description)
-	require.Equal(t, "guid-2", stacks[1].GUID)
-	require.Equal(t, "https://api.example.org/v3/stacks/guid-2", stacks[1].Links["self"].Href)
+			obj, err := tt.Action(cl, t)
+			require.NoError(t, err, details)
+			if tt.Expected != "" {
+				actual, err := json.Marshal(obj)
+				require.NoError(t, err, details)
+				require.JSONEq(t, tt.Expected, string(actual), details)
+			}
+		}()
+	}
 }
--- resource/stacks.go
+++ resource/stacks.go
@@ -2,18 +2,30 @@ package resource
 
 import "time"
 
-// Stack implements stack object. Stacks are the base operating system and file system that your application will execute in. A stack is how you configure applications to run against different operating systems (like Windows or Linux) and different versions of those operating systems.
+// Stack implements stack object. Stacks are the base operating system and file system that your
+// application will execute in. A stack is how you configure applications to run against different
+// operating systems (like Windows or Linux) and different versions of those operating systems.
 type Stack struct {
 	GUID        string          `json:"guid"`
 	CreatedAt   time.Time       `json:"created_at"`
 	UpdatedAt   time.Time       `json:"updated_at"`
-	Name        string          `json:"name,omitempty"`
-	Description string          `json:"description,omitempty"`
-	Links       map[string]Link `json:"links,omitempty"`
-	Metadata    Metadata        `json:"metadata,omitempty"`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Links       map[string]Link `json:"links"`
+	Metadata    *Metadata       `json:"metadata"`
 }
 
-type ListStacksResponse struct {
-	Pagination Pagination `json:"pagination,omitempty"`
-	Resources  []Stack    `json:"resources,omitempty"`
+type StackCreate struct {
+	Name        string    `json:"name"`
+	Description *string   `json:"description,omitempty"`
+	Metadata    *Metadata `json:"metadata,omitempty"`
+}
+
+type StackUpdate struct {
+	Metadata *Metadata `json:"metadata,omitempty"`
+}
+
+type StackList struct {
+	Pagination Pagination `json:"pagination"`
+	Resources  []*Stack   `json:"resources"`
 }
