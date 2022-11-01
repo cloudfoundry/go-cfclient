430c8bbaa333184098060dbbf22641c0f815ab5a
Shawn Neal <sneal@sneal.net>
2022-11-01 12:35:42 -0700

    Add updated service credential binding client

--- client/service_credential_bindings.go
+++ client/service_credential_bindings.go
@@ -1,85 +1,171 @@
 package client
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-
 	"github.com/cloudfoundry-community/go-cfclient/resource"
+	"net/url"
 )
 
 type ServiceCredentialBindingClient commonClient
 
-// List retrieves all service credential bindings
-func (c *ServiceCredentialBindingClient) List() ([]resource.ServiceCredentialBindings, error) {
-	return c.ListByQuery(nil)
+// ServiceCredentialBindingListOptions list filters
+type ServiceCredentialBindingListOptions struct {
+	*ListOptions
+
+	Names                Filter `filter:"names,omitempty"`                  // list of service credential binding names to filter by
+	ServiceInstanceGUIDs Filter `filter:"service_instance_guids,omitempty"` // list of SI guids to filter by
+	ServiceInstanceNames Filter `filter:"service_instance_names,omitempty"` // list of SI names to filter by
+	AppGUIDs             Filter `filter:"app_guids,omitempty"`              // list of app guids to filter by
+	AppNames             Filter `filter:"app_names,omitempty"`              // list of app names to filter by
+	ServicePlanGUIDs     Filter `filter:"service_plan_guids,omitempty"`     // list of service plan guids to filter by
+	ServicePlanNames     Filter `filter:"service_plan_names,omitempty"`     // list of service plan names to filter by
+	ServiceOfferingGUIDs Filter `filter:"service_offering_guids,omitempty"` // list of service offering guids to filter by
+	ServiceOfferingNames Filter `filter:"service_offering_names,omitempty"` // list of service offering names to filter by
+	Type                 Filter `filter:"type,omitempty"`                   // list of service credential binding types to filter by, app or key
+	GUIDs                Filter `filter:"guids,omitempty"`                  // list of service route binding guids to filter by
 }
 
-// ListByQuery retrieves service credential bindings using a query
-func (c *ServiceCredentialBindingClient) ListByQuery(query url.Values) ([]resource.ServiceCredentialBindings, error) {
-	var svcCredentialBindings []resource.ServiceCredentialBindings
-	requestURL := "/v3/service_credential_bindings"
-	if e := query.Encode(); len(e) > 0 {
-		requestURL += "?" + e
+// NewServiceCredentialBindingListOptions creates new options to pass to list
+func NewServiceCredentialBindingListOptions() *ServiceCredentialBindingListOptions {
+	return &ServiceCredentialBindingListOptions{
+		ListOptions: NewListOptions(),
 	}
+}
 
+func (o ServiceCredentialBindingListOptions) ToQueryString() url.Values {
+	return o.ListOptions.ToQueryString(o)
+}
+
+// ServiceCredentialBindingListIncludeOptions list filters
+type ServiceCredentialBindingListIncludeOptions struct {
+	*ServiceCredentialBindingListOptions
+
+	Include resource.ServiceCredentialBindingIncludeType `filter:"include,omitempty"`
+}
+
+// NewServiceCredentialBindingListIncludeOptions creates new options to pass to list
+func NewServiceCredentialBindingListIncludeOptions(include resource.ServiceCredentialBindingIncludeType) *ServiceCredentialBindingListIncludeOptions {
+	return &ServiceCredentialBindingListIncludeOptions{
+		Include:                             include,
+		ServiceCredentialBindingListOptions: NewServiceCredentialBindingListOptions(),
+	}
+}
+
+func (o ServiceCredentialBindingListIncludeOptions) ToQueryString() url.Values {
+	u := o.ServiceCredentialBindingListOptions.ToQueryString()
+	if o.Include != resource.ServiceCredentialBindingIncludeNone {
+		u.Set("include", o.Include.String())
+	}
+	return u
+}
+
+// Create a new service credential binding
+func (c *ServiceCredentialBindingClient) Create(r *resource.ServiceCredentialBindingCreate) (*resource.ServiceCredentialBinding, error) {
+	var d resource.ServiceCredentialBinding
+	err := c.client.post("", "/v3/service_credential_bindings", r, &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+// Delete the specified service credential binding
+func (c *ServiceCredentialBindingClient) Delete(guid string) error {
+	return c.client.delete(path("/v3/service_credential_bindings/%s", guid))
+}
+
+// Get the specified service credential binding
+func (c *ServiceCredentialBindingClient) Get(guid string) (*resource.ServiceCredentialBinding, error) {
+	var d resource.ServiceCredentialBinding
+	err := c.client.get(path("/v3/service_credential_bindings/%s", guid), &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+// GetInclude allows callers to fetch a service credential binding and include information of parent objects in the response
+func (c *ServiceCredentialBindingClient) GetInclude(guid string, include resource.ServiceCredentialBindingIncludeType) (*resource.ServiceCredentialBinding, *resource.ServiceCredentialBindingIncluded, error) {
+	var r resource.ServiceCredentialBindingWithIncluded
+	err := c.client.get(path("/v3/service_credential_bindings/%s?include=%s", guid, include), &r)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &r.ServiceCredentialBinding, r.Included, nil
+}
+
+// List pages ServiceCredentialBindings the user has access to
+func (c *ServiceCredentialBindingClient) List(opts *ServiceCredentialBindingListOptions) ([]*resource.ServiceCredentialBinding, *Pager, error) {
+	var res resource.ServiceCredentialBindingList
+	err := c.client.get(path("/v3/service_credential_bindings?%s", opts.ToQueryString()), &res)
+	if err != nil {
+		return nil, nil, err
+	}
+	pager := NewPager(res.Pagination)
+	return res.Resources, pager, nil
+}
+
+// ListAll retrieves all ServiceCredentialBindings the user has access to
+func (c *ServiceCredentialBindingClient) ListAll(opts *ServiceCredentialBindingListOptions) ([]*resource.ServiceCredentialBinding, error) {
+	if opts == nil {
+		opts = NewServiceCredentialBindingListOptions()
+	}
+	return AutoPage[*ServiceCredentialBindingListOptions, *resource.ServiceCredentialBinding](opts, func(opts *ServiceCredentialBindingListOptions) ([]*resource.ServiceCredentialBinding, *Pager, error) {
+		return c.List(opts)
+	})
+}
+
+// ListInclude page all service credential bindings the user has access to and include the specified parent resources
+func (c *ServiceCredentialBindingClient) ListInclude(opts *ServiceCredentialBindingListIncludeOptions) ([]*resource.ServiceCredentialBinding, *resource.ServiceCredentialBindingIncluded, *Pager, error) {
+	if opts == nil {
+		opts = NewServiceCredentialBindingListIncludeOptions(resource.ServiceCredentialBindingIncludeNone)
+	}
+	var res resource.ServiceCredentialBindingList
+	err := c.client.get(path("/v3/service_credential_bindings?%s", opts.ToQueryString()), &res)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	pager := NewPager(res.Pagination)
+	return res.Resources, res.Included, pager, nil
+}
+
+// ListIncludeAll retrieves all service credential bindings the user has access to and include the specified parent resources
+func (c *ServiceCredentialBindingClient) ListIncludeAll(opts *ServiceCredentialBindingListIncludeOptions) ([]*resource.ServiceCredentialBinding, *resource.ServiceCredentialBindingIncluded, error) {
+	if opts == nil {
+		opts = NewServiceCredentialBindingListIncludeOptions(resource.ServiceCredentialBindingIncludeNone)
+	}
+	return serviceCredentialBindingAutoPageInclude[*ServiceCredentialBindingListIncludeOptions, *resource.ServiceCredentialBinding](opts,
+		func(opts *ServiceCredentialBindingListIncludeOptions) ([]*resource.ServiceCredentialBinding, *resource.ServiceCredentialBindingIncluded, *Pager, error) {
+			return c.ListInclude(opts)
+		})
+}
+
+// Update the specified attributes of the app
+func (c *ServiceCredentialBindingClient) Update(guid string, r *resource.ServiceCredentialBindingUpdate) (*resource.ServiceCredentialBinding, error) {
+	var d resource.ServiceCredentialBinding
+	err := c.client.patch(path("/v3/service_credential_bindings/%s", guid), r, &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+type serviceCredentialBindingListIncludeFunc[T ListOptioner, R any] func(opts T) ([]R, *resource.ServiceCredentialBindingIncluded, *Pager, error)
+
+func serviceCredentialBindingAutoPageInclude[T ListOptioner, R any](opts T, list serviceCredentialBindingListIncludeFunc[T, R]) ([]R, *resource.ServiceCredentialBindingIncluded, error) {
+	var all []R
+	var allIncluded *resource.ServiceCredentialBindingIncluded
 	for {
-		r := c.client.NewRequest("GET", requestURL)
-		resp, err := c.client.DoRequest(r)
+		page, included, pager, err := list(opts)
 		if err != nil {
-			return nil, fmt.Errorf("error requesting service credential bindings: %w", err)
+			return nil, nil, err
 		}
-		defer func(b io.ReadCloser) {
-			_ = b.Close()
-		}(resp.Body)
-
-		if resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("error listing service credential bindings, response code: %d", resp.StatusCode)
-		}
-
-		var data resource.ListServiceCredentialBindingsResponse
-		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-			return nil, fmt.Errorf("error parsing JSON from list service credential bindings: %w", err)
-		}
-
-		svcCredentialBindings = append(svcCredentialBindings, data.Resources...)
-
-		requestURL = data.Pagination.Next.Href
-		if requestURL == "" || query.Get("page") != "" {
+		all = append(all, page...)
+		allIncluded.ServiceInstances = append(allIncluded.ServiceInstances, included.ServiceInstances...)
+		allIncluded.Apps = append(allIncluded.Apps, included.Apps...)
+		if !pager.HasNextPage() {
 			break
 		}
-		requestURL, err = extractPathFromURL(requestURL)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing the next page request url for service credential bindings: %w", err)
-		}
+		pager.NextPage(opts)
 	}
-
-	return svcCredentialBindings, nil
-}
-
-// Get retrieves the service credential binding based on the provided guid
-func (c *ServiceCredentialBindingClient) Get(GUID string) (*resource.ServiceCredentialBindings, error) {
-	requestURL := fmt.Sprintf("/v3/service_credential_bindings/%s", GUID)
-	req := c.client.NewRequest("GET", requestURL)
-	resp, err := c.client.DoRequest(req)
-
-	if err != nil {
-		return nil, fmt.Errorf("error while getting service credential binding: %w", err)
-	}
-	defer func(b io.ReadCloser) {
-		_ = b.Close()
-	}(resp.Body)
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("error getting service credential binding with GUID [%s], response code: %d", GUID, resp.StatusCode)
-	}
-
-	var svcCredentialBindings resource.ServiceCredentialBindings
-	if err := json.NewDecoder(resp.Body).Decode(&svcCredentialBindings); err != nil {
-		return nil, fmt.Errorf("error reading service credential binding JSON: %w", err)
-	}
-
-	return &svcCredentialBindings, nil
+	return all, allIncluded, nil
 }
--- client/service_credential_bindings_test.go
+++ client/service_credential_bindings_test.go
@@ -1,46 +1,141 @@
 package client
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/cloudfoundry-community/go-cfclient/resource"
+	"github.com/cloudfoundry-community/go-cfclient/test"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"testing"
 )
 
-func TestListServiceCredentialBindingsByQuery(t *testing.T) {
-	setup(MockRoute{"GET", "/v3/service_credential_bindings", []string{listServiceCredentialBindingsPayload}, "", http.StatusOK, "", ""}, t)
-	defer teardown()
+func TestServiceCredentialBindings(t *testing.T) {
+	g := test.NewObjectJSONGenerator(1)
+	scb := g.ServiceCredentialBinding()
+	scb2 := g.ServiceCredentialBinding()
 
-	c, _ := NewTokenConfig(server.URL, "foobar")
-	client, err := New(c)
-	require.NoError(t, err)
+	tests := []RouteTest{
+		{
+			Description: "Create service credential binding",
+			Route: MockRoute{
+				Method:   "POST",
+				Endpoint: "/v3/service_credential_bindings",
+				Output:   []string{scb},
+				Status:   http.StatusCreated,
+				PostForm: `{
+					"type": "app",
+					"name": "some-binding-name",
+					"relationships": {
+					  "service_instance": {
+						"data": {
+						  "guid": "7304bc3c-7010-11ea-8840-48bf6bec2d78"
+						}
+					  },
+					  "app": {
+						"data": {
+						  "guid": "e0e4417c-74ee-11ea-a604-48bf6bec2d78"
+						}
+					  }
+					},
+					"parameters": {
+					  "key1": "value1",
+					  "key2": "value2"
+					}
+				  }`,
+			},
+			Expected: scb,
+			Action: func(c *Client, t *testing.T) (any, error) {
+				r := resource.NewServiceCredentialBindingCreateApp(
+					"7304bc3c-7010-11ea-8840-48bf6bec2d78", "e0e4417c-74ee-11ea-a604-48bf6bec2d78").
+					WithName("some-binding-name").
+					WithJSONParameters(`{
+					  "key1": "value1",
+					  "key2": "value2"
+					}`)
+				return c.ServiceCredentialBindings.Create(r)
+			},
+		},
+		{
+			Description: "Delete service credential binding",
+			Route: MockRoute{
+				Method:   "DELETE",
+				Endpoint: "/v3/service_credential_bindings/59ba6d78-6a21-4321-83a9-f7eacd88b08d",
+				Status:   http.StatusAccepted,
+			},
+			Action: func(c *Client, t *testing.T) (any, error) {
+				return nil, c.ServiceCredentialBindings.Delete("59ba6d78-6a21-4321-83a9-f7eacd88b08d")
+			},
+		},
+		{
+			Description: "Get service credential binding",
+			Route: MockRoute{
+				Method:   "GET",
+				Endpoint: "/v3/service_credential_bindings/59ba6d78-6a21-4321-83a9-f7eacd88b08d",
+				Output:   []string{scb},
+				Status:   http.StatusOK},
+			Expected: scb,
+			Action: func(c *Client, t *testing.T) (any, error) {
+				return c.ServiceCredentialBindings.Get("59ba6d78-6a21-4321-83a9-f7eacd88b08d")
+			},
+		},
+		{
+			Description: "List all service credential bindings",
+			Route: MockRoute{
+				Method:   "GET",
+				Endpoint: "/v3/service_credential_bindings",
+				Output:   g.Paged([]string{scb}, []string{scb2}),
+				Status:   http.StatusOK},
+			Expected: g.Array(scb, scb2),
+			Action: func(c *Client, t *testing.T) (any, error) {
+				return c.ServiceCredentialBindings.ListAll(nil)
+			},
+		},
+		{
+			Description: "Update service credential binding",
+			Route: MockRoute{
+				Method:   "PATCH",
+				Endpoint: "/v3/service_credential_bindings/59ba6d78-6a21-4321-83a9-f7eacd88b08d",
+				Output:   []string{scb},
+				Status:   http.StatusOK,
+				PostForm: `{ "metadata": { "labels": {"foo": "bar"}, "annotations": {"baz": "qux"} }}`,
+			},
+			Expected: scb,
+			Action: func(c *Client, t *testing.T) (any, error) {
+				r := &resource.ServiceCredentialBindingUpdate{
+					Metadata: resource.Metadata{
+						Labels: map[string]string{
+							"foo": "bar",
+						},
+						Annotations: map[string]string{
+							"baz": "qux",
+						},
+					},
+				}
+				return c.ServiceCredentialBindings.Update("59ba6d78-6a21-4321-83a9-f7eacd88b08d", r)
+			},
+		},
+	}
+	for _, tt := range tests {
+		func() {
+			setup(tt.Route, t)
+			defer teardown()
+			details := fmt.Sprintf("%s %s", tt.Route.Method, tt.Route.Endpoint)
+			if tt.Description != "" {
+				details = tt.Description + ": " + details
+			}
 
-	serviceCredentialsBindings, err := client.ServiceCredentialBindings.List()
-	require.NoError(t, err)
-	require.Len(t, serviceCredentialsBindings, 1)
+			c, _ := NewTokenConfig(server.URL, "foobar")
+			cl, err := New(c)
+			require.NoError(t, err, details)
 
-	require.Equal(t, "my_service_key", serviceCredentialsBindings[0].Name)
-	require.Equal(t, "key", serviceCredentialsBindings[0].Type)
-
-	require.Equal(t, "85ccdcad-d725-4109-bca4-fd6ba062b5c8", serviceCredentialsBindings[0].Relationships["service_instance"].Data.GUID)
-	require.Equal(t, "https://api.example.org/v3/service_instances/85ccdcad-d725-4109-bca4-fd6ba062b5c8", serviceCredentialsBindings[0].Links["service_instance"].Href)
-}
-
-func TestGetServiceCredentialBindingsByGUID(t *testing.T) {
-	GUID := "d9634934-8e1f-4c2d-bb33-fa5df019cf9d"
-	setup(MockRoute{"GET", fmt.Sprintf("/v3/service_credential_bindings/%s", GUID), []string{getServiceCredentialBindingsByGUIDPayload}, "", http.StatusOK, "", ""}, t)
-	defer teardown()
-
-	c, _ := NewTokenConfig(server.URL, "foobar")
-	client, err := New(c)
-	require.NoError(t, err)
-
-	serviceCredentialsBinding, err := client.ServiceCredentialBindings.Get(GUID)
-	require.NoError(t, err)
-
-	require.Equal(t, "my_service_key", serviceCredentialsBinding.Name)
-	require.Equal(t, "key", serviceCredentialsBinding.Type)
-
-	require.Equal(t, "85ccdcad-d725-4109-bca4-fd6ba062b5c8", serviceCredentialsBinding.Relationships["service_instance"].Data.GUID)
-	require.Equal(t, "https://api.example.org/v3/service_instances/85ccdcad-d725-4109-bca4-fd6ba062b5c8", serviceCredentialsBinding.Links["service_instance"].Href)
+			obj, err := tt.Action(cl, t)
+			require.NoError(t, err, details)
+			if tt.Expected != "" {
+				actual, err := json.Marshal(obj)
+				require.NoError(t, err, details)
+				require.JSONEq(t, tt.Expected, string(actual), details)
+			}
+		}()
+	}
 }
