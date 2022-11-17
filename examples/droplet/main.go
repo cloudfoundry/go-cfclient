bd3d7d625b76da9bde251e319915d934d3ea8c4e
Shawn Neal <sneal@sneal.net>
2022-11-16 20:13:28 -0800

    Add support for external blob stores and fix #295
    Use the base unauthenticated client provider for downloading blobs. This avoids sending any unwanted Authorization header while requesting blobs and ensures all http.Client settings are properly preserved from the config.

---
+++ examples/droplet/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/cloudfoundry-community/go-cfclient/v3/client"
+	"github.com/cloudfoundry-community/go-cfclient/v3/config"
+	"io"
+	"os"
+)
+
+func main() {
+	err := execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Done!")
+}
+
+func execute() error {
+	conf, err := config.NewFromCFHome()
+	if err != nil {
+		return err
+	}
+	conf.WithSkipTLSValidation(true)
+	cf, err := client.New(conf)
+	if err != nil {
+		return err
+	}
+
+	droplets, err := cf.Droplets.ListAll(nil)
+	if err != nil {
+		return err
+	}
+	if len(droplets) < 1 {
+		return errors.New("error listing droplets, expected at least one droplet")
+	}
+	droplet := droplets[0]
+
+	reader, err := cf.Droplets.Download(droplet.GUID)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = reader.Close() }()
+
+	dropletFile, err := os.CreateTemp("", "droplet-*.zip")
+	if err != nil {
+		return err
+	}
+	defer func() { _ = dropletFile.Close() }()
+
+	fmt.Printf("Writing droplet %s to %s\n", droplet.GUID, dropletFile.Name())
+	_, err = io.Copy(dropletFile, reader)
+	return err
+}
