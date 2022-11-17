932e9a569ebe729c094a50b8d8849017320a095e
Shawn Neal <sneal@sneal.net>
2022-11-16 21:09:57 -0800

    Add fix for error when applying a manifest to a space when pushing an app
    Add an updated push app example that uses the push app operation

--- examples/push_app/main.go
+++ examples/push_app/main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"github.com/cloudfoundry-community/go-cfclient/v3/client"
 	"github.com/cloudfoundry-community/go-cfclient/v3/config"
-	"github.com/cloudfoundry-community/go-cfclient/v3/resource"
+	"github.com/cloudfoundry-community/go-cfclient/v3/operation"
+	"gopkg.in/yaml.v3"
 	"os"
 )
 
 func main() {
-	err := runPush()
+	if len(os.Args) != 4 {
+		fmt.Println("expected arguments: org, space, /path/to/spring-music.jar")
+		os.Exit(1)
+	}
+	org := os.Args[1]
+	space := os.Args[2]
+	pathToZip := os.Args[3]
+
+	err := runPush(org, space, pathToZip)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -17,7 +26,7 @@ func main() {
 	fmt.Println("Done!")
 }
 
-func runPush() error {
+func runPush(org, space, pathToZip string) error {
 	conf, err := config.NewFromCFHome()
 	if err != nil {
 		return err
@@ -28,102 +37,29 @@ func runPush() error {
 		return err
 	}
 
-	spaceOpts := client.NewSpaceListOptions()
-	spaceOpts.Names.Values = []string{"dev"}
-	spaces, err := cf.Spaces.ListAll(spaceOpts)
-	if err != nil {
-		return err
-	}
-	if len(spaces) != 1 {
-		return fmt.Errorf("expected one space named dev, but found %d", len(spaces))
-	}
-	space := spaces[0]
-
-	jobGUID, err := cf.Manifests.ApplyManifest(space.GUID, manifest)
-	if err != nil {
-		return err
-	}
-	err = cf.Jobs.PollComplete(jobGUID, nil)
+	var manifest *operation.Manifest
+	err = yaml.Unmarshal([]byte(yamlManifest), &manifest)
 	if err != nil {
 		return err
 	}
 
-	appOpts := client.NewAppListOptions()
-	appOpts.Names.Values = []string{"spring-music"}
-	apps, err := cf.Applications.ListAll(appOpts)
+	zipFile, err := os.Open(pathToZip)
 	if err != nil {
 		return err
 	}
-	if len(apps) != 1 {
-		return fmt.Errorf("expected one space named dev, but found %d", len(apps))
-	}
-	app := apps[0]
-
-	newPkg := resource.NewPackageCreate(app.GUID)
-	pkg, err := cf.Packages.Create(newPkg)
+	pushOp := operation.NewAppPushOperation(cf, org, space)
+	app, err := pushOp.Push(manifest.Applications[0], zipFile)
 	if err != nil {
 		return err
 	}
-
-	// might need full path or copy it locally
-	f, err := os.Open("spring-music-1.0.jar")
-	if err != nil {
-		return err
-	}
-	err = cf.Packages.UploadBits(pkg.GUID, f)
-	if err != nil {
-		return err
-	}
-	err = cf.Packages.PollReady(pkg.GUID, nil)
-	if err != nil {
-		return err
-	}
-
-	newBuild := resource.NewBuildCreate(pkg.GUID)
-	newBuild.Lifecycle = &resource.Lifecycle{
-		Type: "buildpack",
-		BuildpackData: resource.BuildpackLifecycle{
-			Buildpacks: []string{"java_buildpack_offline"},
-			Stack:      "cflinuxfs3",
-		},
-	}
-	build, err := cf.Builds.Create(newBuild)
-	if err != nil {
-		return err
-	}
-	err = cf.Builds.PollStaged(build.GUID, nil)
-	if err != nil {
-		return err
-	}
-
-	droplets, err := cf.Droplets.ListForPackageAll(pkg.GUID, nil)
-	if err != nil {
-		return err
-	}
-	if len(droplets) != 1 {
-		return fmt.Errorf("expected one droplet, but found %d", len(droplets))
-	}
-	droplet := droplets[0]
-
-	_, err = cf.Droplets.SetCurrentAssociationForApp(app.GUID, droplet.GUID)
-	if err != nil {
-		return err
-	}
-
-	app, err = cf.Applications.Start(app.GUID)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Finished pushing %s\n", app.Name)
-
+	fmt.Printf("successfully pushed %s, state: %s\n", app.Name, app.State)
 	return nil
 }
 
-const manifest = `
+const yamlManifest = `
 ---
 applications:
-- name: spring-music
+- name: spring-music-example
   memory: 1G
   random-route: true
   stack: cflinuxfs3
