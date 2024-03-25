ff22e4957623d1c311dd4a52394c706facaee78c
ckganesan <kbganesan87@gmail.com>
2024-03-25 12:33:22 +0530

    Implemented additional test cases for unmarshalling the manifest

--- operation/manifest_test.go
+++ operation/manifest_test.go
@@ -5,7 +5,66 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
+	yamlv3 "gopkg.in/yaml.v3"
 )
+
+const fullSpringMusicYaml = `applications:
+- name: spring-music
+  buildpacks:
+  - java_buildpack_offline
+  env:
+    SPRING_CLOUD_PROFILE: dev
+  routes:
+  - route: spring-music-egregious-porcupine-oa.apps.example.org
+  services:
+  - name: my-sql
+  stack: cflinuxfs3
+  command: java
+  disk_quota: 1G
+  health-check-type: http
+  health-check-http-endpoint: /health
+  instances: 2
+  log-rate-limit-per-second: 100MB
+  memory: 1G
+  timeout: 60
+`
+
+const minimalSpringMusicYaml = `applications:
+- name: spring-music
+  buildpacks:
+  - java_buildpack_offline
+  no-route: true
+  stack: cflinuxfs3
+  health-check-type: port
+  health-check-http-endpoint: /
+  instances: 1
+  memory: 1G
+`
+
+const fullSpringMusicYamlV2 = `applications:
+  - name: spring-music
+    buildpacks:
+      - java_buildpack_offline
+    env:
+      SPRING_CLOUD_PROFILE: dev
+    routes:
+      - route: spring-music-egregious-porcupine-oa.apps.example.org
+    services:
+      - name: my-sql
+        binding_name: mysql
+        parameters:
+          name: mysql
+      - oauth2
+    stack: cflinuxfs3
+    command: java
+    disk_quota: 1G
+    health-check-type: http
+    health-check-http-endpoint: /health
+    instances: 2
+    log-rate-limit-per-second: 100MB
+    memory: 1G
+    timeout: 60
+`
 
 func TestManifestMarshalling(t *testing.T) {
 	m := &Manifest{
@@ -55,35 +114,24 @@ func TestManifestMarshalling(t *testing.T) {
 	require.Equal(t, minimalSpringMusicYaml, string(b))
 }
 
-const fullSpringMusicYaml = `applications:
-- name: spring-music
-  buildpacks:
-  - java_buildpack_offline
-  env:
-    SPRING_CLOUD_PROFILE: dev
-  routes:
-  - route: spring-music-egregious-porcupine-oa.apps.example.org
-  services:
-  - name: my-sql
-  stack: cflinuxfs3
-  command: java
-  disk_quota: 1G
-  health-check-type: http
-  health-check-http-endpoint: /health
-  instances: 2
-  log-rate-limit-per-second: 100MB
-  memory: 1G
-  timeout: 60
-`
+func TestManifestUnMarshalling(t *testing.T) {
+	var m Manifest
 
-const minimalSpringMusicYaml = `applications:
-- name: spring-music
-  buildpacks:
-  - java_buildpack_offline
-  no-route: true
-  stack: cflinuxfs3
-  health-check-type: port
-  health-check-http-endpoint: /
-  instances: 1
-  memory: 1G
-`
+	err := yaml.Unmarshal([]byte(fullSpringMusicYamlV2), &m)
+	require.NoError(t, err)
+	require.Equal(t, "my-sql", (*m.Applications[0].Services)[0].Name)
+	require.Equal(t, "mysql", (*m.Applications[0].Services)[0].BindingName)
+	require.Equal(t, "mysql", (*m.Applications[0].Services)[0].Parameters["name"])
+	require.Equal(t, "oauth2", (*m.Applications[0].Services)[1].Name)
+	require.Equal(t, 1, len(m.Applications))
+	require.Equal(t, 2, len(*m.Applications[0].Services))
+
+	err = yamlv3.Unmarshal([]byte(fullSpringMusicYamlV2), &m)
+	require.NoError(t, err)
+	require.Equal(t, "my-sql", (*m.Applications[0].Services)[0].Name)
+	require.Equal(t, "mysql", (*m.Applications[0].Services)[0].BindingName)
+	require.Equal(t, "mysql", (*m.Applications[0].Services)[0].Parameters["name"])
+	require.Equal(t, "oauth2", (*m.Applications[0].Services)[1].Name)
+	require.Equal(t, 1, len(m.Applications))
+	require.Equal(t, 2, len(*m.Applications[0].Services))
+}
