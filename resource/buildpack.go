c8dab58d2ce8fc4bbba937777a2c38495832f71b
Debaditya Ray <debadityaray00@gmail.com>
2024-06-20 15:09:51 +0530

    fix: Make stack in buildpack pointer
    Signed-off-by: Ray <debaditya.ray@sap.com>

--- resource/buildpack.go
+++ resource/buildpack.go
@@ -4,7 +4,7 @@ type Buildpack struct {
 	Name     string  `json:"name"`     // The name of the buildpack; to be used by app buildpack field (only alphanumeric characters)
 	State    string  `json:"state"`    // The state of the buildpack Valid value is: AWAITING_UPLOAD
 	Filename *string `json:"filename"` // The filename of the buildpack, if any
-	Stack    string  `json:"stack"`    // The name of the stack that the buildpack will use
+	Stack    *string `json:"stack"`    // The name of the stack that the buildpack will use
 	Position int     `json:"position"` // The order in which the buildpacks are checked during buildpack auto-detection
 	Enabled  bool    `json:"enabled"`  // Whether the buildpack can be used for staging
 	Locked   bool    `json:"locked"`   // Whether the buildpack is locked to prevent updating the bits
